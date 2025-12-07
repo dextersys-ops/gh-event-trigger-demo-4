f1045eb7fb7c97403a183fee74793a99299d36a2
dextersys-ops <shriramnath.moond@gmail.com>
2025-12-07 17:56:22 +0530

    Update description of GitHub in HelpArea component

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -10,7 +10,7 @@ const HELP_ITEMS = [
   {
     id: 'h2',
     title: 'What is GitHub?',
-    text: 'GitHub is a company and online offering, providing you with tons of Git-related services (e.g., cloud repositories).',
+    text: 'GitHub is a cloud-based platform for hosting, managing, and collaborating on software projects using Git, the most widely used version control system',
   },
   {
     id: 'h3',
